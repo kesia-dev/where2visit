1f723b144a422cc5a5d3118c0e83a44881e7109b
iDebonair <ofemiowolabi@gmail.com>
2023-12-02 23:51:54 -0700

    set up homepage, create room, join room pages and components. Included user authentication

---
+++ src/component/Navbar.js
@@ -0,0 +1,28 @@
+// Navbar.js
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Navbar = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleSignIn = () => {
+    // Redirect the user to the Auth0 login page
+    loginWithRedirect();
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Where2Visit
+        </Typography>
+        <Button color="inherit" onClick={handleSignIn}>
+          Sign In
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
