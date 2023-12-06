65615711c1337a8689e364209916a3e21f06effd
iDebonair <ofemiowolabi@gmail.com>
2023-12-06 16:38:50 -0700

    modify create room

--- frontend/src/component/RoomInfo.js
+++ frontend/src/component/RoomInfo.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Container, Typography, Paper, Box, IconButton, Tooltip, Snackbar  } from '@mui/material';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Container, Typography, Paper, Box, IconButton, Tooltip, Snackbar, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import copy from 'clipboard-copy';
 
 const RoomInfo = () => {
-  // Extract the roomCode from the URL parameters
   const { roomCode } = useParams();
-  
   const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const navigate = useNavigate();
 
   const handleCopyToClipboard = () => {
     copy(roomCode);
@@ -17,6 +17,22 @@ const RoomInfo = () => {
 
   const handleCloseSnackbar = () => {
     setIsSnackbarOpen(false);
+  };
+
+  const handleCloseRoom = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleConfirmCloseRoom = () => {
+    // Add logic to perform any necessary cleanup or notify the server
+    // before closing the room, then navigate back to the home route
+    // validate();
+    navigate('/');
+    setIsDialogOpen(false);
+  };
+
+  const handleCancelCloseRoom = () => {
+    setIsDialogOpen(false);
   };
 
   return (
@@ -37,12 +53,15 @@ const RoomInfo = () => {
             Room Code: {roomCode}
             {/* Copy to Clipboard Button */}
             <Tooltip title="Copy to Clipboard" arrow>
-                <IconButton onClick={handleCopyToClipboard} size="small" style={{ marginLeft: '8px' }}>
+              <IconButton onClick={handleCopyToClipboard} size="small" style={{ marginLeft: '8px' }}>
                 <FileCopyIcon />
-                </IconButton>
+              </IconButton>
             </Tooltip>
           </Typography>
           {/* Add additional content or components related to the room */}
+          <Button variant="contained" color="primary" style={{ backgroundColor: 'red' }} onClick={handleCloseRoom} mt={2}>
+            Close Room
+          </Button>
         </Paper>
       </Box>
       <Snackbar
@@ -51,6 +70,27 @@ const RoomInfo = () => {
         onClose={handleCloseSnackbar}
         message="Room code has been copied to clipboard"
       />
+      <Dialog
+        open={isDialogOpen}
+        onClose={handleCancelCloseRoom}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Are you sure you want to close the room?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Closing the room will perform any necessary cleanup or notify the server.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancelCloseRoom} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirmCloseRoom} color="primary" autoFocus>
+            Close Room
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
