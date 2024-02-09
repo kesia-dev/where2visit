65b15bb0f557c16bbd75ad370193f6570dec6fea
DavidGir <davidgiroux149@gmail.com>
2024-02-09 17:20:30 -0500

    Add restaurant data with websites, maps and distances
    Each restaurant object is now enriched with additional key details: the official website URL, a static map image URL, and the calculated distance from the user's current location.

--- backend/controllers/googleMapsApiController.js
+++ backend/controllers/googleMapsApiController.js
@@ -1,11 +1,16 @@
 const axios = require('axios');
 require('dotenv').config();
 const { filterRestaurants } = require('../services/restaurantFilterService');
+const { calculateDistance } = require('../services/distanceService');
+const { fetchRestaurantWebsite } = require('../services/websiteLinksService');
+const { getGoogleStaticMap } = require('../services/mapService');
 
 // Function to get the nearest restaurants from the Google Maps Places API:
 exports.getNearestRestaurants = async (req, res) => {
     // Destructure the query parameters:
     const { keyword, location, radius, type } = req.query;
+    // Split user's location into latitude and longitude and convert them to numbers:
+    const [userLat, userLng] = location.split(',').map(Number);
     // Destructure the filters from the query parameters:
     const filters = {
         numberOfRestaurants: req.query.numberOfRestaurants,
@@ -35,13 +40,33 @@ exports.getNearestRestaurants = async (req, res) => {
     try {
         const response = await axios.get(url);
         const results = response.data.results;
+        // Map over the results and calculate the distance between the user's location and the restaurant's location:
+        const resultsWithRestaurantsDetails = await Promise.all(results.map( async (restaurant) => {
+            const website = await fetchRestaurantWebsite(restaurant.place_id, key);
+            const staticMapUrl = getGoogleStaticMap(restaurant.geometry.location.lat, restaurant.geometry.location.lng, key);
+            
+            return {
+                ...restaurant,
+                website,
+                staticMapUrl,
+                distance: calculateDistance(userLat, userLng, restaurant.geometry.location.lat, restaurant.geometry.location.lng)
+            };
+        }));
         // Filter the restaurants results based on the user's preferences using the service function:
-        const filteredResults = filterRestaurants(results, filters);
+        const filteredResults = filterRestaurants(resultsWithRestaurantsDetails, filters);
         // If there are results, log the names of the restaurants and their photos:
         if (filteredResults && filteredResults.length > 0) {
             console.log(`Found ${filteredResults.length} restaurants: ` + '\n');
             filteredResults.forEach((restaurant, restaurantIndex) => {
-                console.log(`${restaurantIndex + 1}: ${restaurant.name} - Rating: ${restaurant.rating || 'N/A'}, Price Level: ${restaurant.price_level || 'N/A'}`);
+                console.log(`${restaurantIndex + 1}: ${restaurant.name}\n` +
+                `Rating: ${restaurant.rating || 'N/A'}\n` + 
+                `Price Level: ${restaurant.price_level || 'N/A'}\n` +
+                `Address: ${restaurant.vicinity || 'N/A'}\n` +
+                `Map URL: ${restaurant.staticMapUrl || 'N/A'}\n` +
+                `Website: ${restaurant.website || 'N/A'}\n` +
+                `Distance from you: ${restaurant.distance.toFixed(2)} km\n` +
+                `Ratings Count: ${restaurant.user_ratings_total || 'N/A'}\n`
+                );
                 // Check if the place has photos:
                 if (restaurant.photos && restaurant.photos.length > 0) {
                     console.log('Photos available: ');
