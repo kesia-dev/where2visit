1a4bd5f390c64098a66b201c05292186cdb247b2
Paulo Campos Filho <pezaum@gmail.com>
2024-02-13 00:41:46 -0800

    Change nodemailerConfig to use gmail as service

--- backend/config/nodemailerConfig.js
+++ backend/config/nodemailerConfig.js
@@ -1,7 +1,5 @@
 module.exports = {
-  host: process.env.NODEMAILER_HOST,
-  port: process.env.NODEMAILER_HOST_PORT,
-  secure: process.env.NODEMAILER_AUTH_IS_SECURE === 'true',
+  service: 'gmail',
   auth: {
     user: process.env.NODEMAILER_AUTH_USER,
     pass: process.env.NODEMAILER_AUTH_PASS,
