0bc7441b5288d7a8a2126dc9341c901996b5f58f
Femi <ofemiowolabi@gmail.com>
2024-02-19 17:30:12 -0700

    modify homepage, join plan and instructions to reflect figma diagram

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react'; // Import Auth0Provider
 import './App.css';
 import JoinPlan from './component/JoinPlan.js';
+import Instructions from './component/Instructions.js';
 
-
-//const domain = process.env.REACT_APP_AUTH0_DOMAIN
-//const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 const domain = "dev-rkjah3umfq8c82at.us.auth0.com"
 const clientId = "xbeT4PtSPVwbkexbBRsCKmzveb3aNIE0"
 
 function App() {
   return (
-    
     <Router>
       <Auth0Provider
         domain={domain}
@@ -25,11 +22,12 @@ function App() {
         }}
       >
         <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/join-plan/:planCode" element={<JoinPlan />} />
-            {/* Add other routes as needed */}
-          </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/join-plan/:planCode" element={<JoinPlan />} />
+          <Route path="/instructions" element={<Instructions />} />
+          {/* Add other routes as needed */}
+        </Routes>
       </Auth0Provider>
     </Router>
   );
--- frontend/src/component/PlanDetails.js
+++ frontend/src/component/PlanDetails.js
@@ -3,28 +3,28 @@ import { Card, CardContent, Typography } from '@mui/material';
 
 const PlanDetails = ({ planName, hostName, dateOfEvent, timeOfEvent, location }) => {
   return (
-    <Card elevation={0} sx={{ marginBottom: '20px', alignItems: 'center', justifyContent: 'center', background: '#E9D8A3', color: 'black', width: '100%'}}>
+    <Card elevation={0} sx={{ marginTop: '20px', alignItems: 'center', justifyContent: 'center', background: '#E9D8A3', color: 'black', width: '430px' }}>
       <CardContent sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-        <Typography variant="h6" gutterBottom sx={{ color: '#333' }}>
+        <Typography variant="h6" gutterBottom sx={{ color: '#333', marginBottom: 2 }}>
           Plan Details
         </Typography>
-        <Typography variant="body1" sx={{ color: '#555' }}>
+        <Typography variant="body1" sx={{ color: '#555', marginBottom: 0.5 }}>
           Room Name: <strong>{planName} </strong>
         </Typography>
-        <Typography variant="body1" sx={{ color: '#555' }}>
+        <Typography variant="body1" sx={{ color: '#555', marginBottom: 0.5 }}>
          Host Name:<strong> {hostName} </strong>
         </Typography>
-        <Typography variant="body1" sx={{ color: '#555' }}>
+        <Typography variant="body1" sx={{ color: '#555', marginBottom: 0.5 }}>
           Date of Event:<strong> {dateOfEvent} </strong>
         </Typography>
-        <Typography variant="body1" sx={{ color: '#555' }}>
+        <Typography variant="body1" sx={{ color: '#555', marginBottom: 0.5 }}>
           Time of Event:<strong> {timeOfEvent} </strong>
         </Typography>
-      {/*
-        <Typography variant="body1" sx={{ color: '#555' }}>
-          <strong>Location:</strong> {location}
-        </Typography>
-      */}
+        {/*
+          <Typography variant="body1" sx={{ color: '#555', marginBottom: 1 }}>
+            <strong>Location:</strong> {location}
+          </Typography>
+        */}
       </CardContent>
     </Card>
   );
