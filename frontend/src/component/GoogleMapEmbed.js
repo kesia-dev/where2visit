24f75f34e79b06ddeee1cacafc3d938c17b5ab6b
Rachel Little <racheldawnlittle@gmail.com>
2024-02-24 21:37:49 -0700

    remove unnecessary imports and comments

--- frontend/src/component/GoogleMapEmbed.js
+++ frontend/src/component/GoogleMapEmbed.js
@@ -1,10 +1,7 @@
 import React from "react";
-// import { getGoogleMapsEmbedUrl } from "../../../backend/services/mapService"
 import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import "../App.css";
+
 const GoogleMapEmbed = ({ googleEmbedMapUrl }) => {
-  //   const embedUrl = getGoogleMapsEmbedUrl(restaurantLat, restaurantLng, key);
 
   return (
     <Card
@@ -28,30 +25,7 @@ const GoogleMapEmbed = ({ googleEmbedMapUrl }) => {
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-      {/* <Card
-        sx={{
-          justifyContent: "center",
-          alignItems: "center",
-          m: 1,
-          p: 0,
-          borderRadius: "16px",
-          border: "2px solid black",
-        }}
-      >
-        <CardMedia
-          component="iframe"
-          src={googleEmbedMapUrl}
-          alt="Map"
-          width="100dvb"
-          height="350px"
-          referrerPolicy="no-referrer-when-downgrade"
-          loading="lazy"
-          allowFullScreen
-          sx={{ border: "none" }}
-          className="google-map-embed"
-        />
-      </Card> */}
+      />
     </Card>
   );
 };
