03c051d36d981bf28734ce13d8c900c1acf6b24f
DavidGir <davidgiroux149@gmail.com>
2024-02-29 21:15:12 -0500

    Refactor WaitingPage component

--- frontend/src/component/WaitingPage.js
+++ frontend/src/component/WaitingPage.js
@@ -1,13 +1,25 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
 import ThumbUpTwoToneIcon from "@mui/icons-material/ThumbUpTwoTone";
-
+import { useNavigate, useParams } from "react-router-dom";
+import useWebSocket from "../hooks/useWebSocket";
 
 export default function WaitingPage() {
+  const navigate = useNavigate();
+  const { planCode } = useParams();
+  const { sessionActive } = useWebSocket("ws://localhost:4200");
+  
+  // If the session is no longer active (time is up or ended by host), redirect to the final poll results page:
+  useEffect(() => {
+    if (!sessionActive) {
+      navigate(`/final-poll/${planCode}`);
+    }
+  }, [sessionActive, planCode, navigate]);
+
   return (
     <Container
       component="main"
@@ -25,10 +37,12 @@ export default function WaitingPage() {
           sx={{ justifyContent: "center", textAlign: "center", mt: "160px" }}
         >
           <Typography variant="h4" gutterBottom>
-            Voting still in session!
+            { sessionActive ? "Voting still in session!" : "Session has ended!" }
           </Typography>
           <Typography variant="h6">
-            Please wait a moment for everyone to finish voting.
+          {sessionActive
+              ? "Please wait a moment for everyone to finish voting."
+              : "Redirecting to the final poll results..."}
           </Typography>
         </Box>
         <Box sx={{ display: "flex", justifyContent: "center", mt: "100px" }}>
