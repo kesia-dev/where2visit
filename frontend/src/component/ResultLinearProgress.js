b078c829d60d21fc7430cd99587ab4a41a15e28b
Rachel Little <racheldawnlittle@gmail.com>
2024-03-04 15:26:13 -0700

    remove console logs

--- frontend/src/component/ResultLinearProgress.js
+++ frontend/src/component/ResultLinearProgress.js
@@ -8,11 +8,8 @@ export default function ResultLinearProgress({
   // Normalize the progress bar value
   const MIN = 0;
   const MAX = totalVoteCount;
-  console.log("Positive vote count:", positiveVoteCount);
-    console.log("Total vote count:", totalVoteCount);
   const normalize = (value) => ((value - MIN) * 100) / (MAX - MIN);
   const normalizedProgress = normalize(positiveVoteCount);
-  console.log("Normalized progress:", normalizedProgress);
 
   return (
     <React.Fragment>
