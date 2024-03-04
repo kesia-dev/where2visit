ce225a067d2747cc394f1ee72c6b563d6d6edecf
DavidGir <davidgiroux149@gmail.com>
2024-03-04 12:31:48 -0500

    Remove unused useState import in WaitingPage.js

--- frontend/src/component/WaitingPage.js
+++ frontend/src/component/WaitingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
