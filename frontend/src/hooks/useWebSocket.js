4b23a3b0622502ba5b1caaa9483201de109abe90
DavidGir <davidgiroux149@gmail.com>
2024-03-04 02:51:42 -0500

    Refactor useWebSocket hook to include planCode parameter

--- frontend/src/hooks/useWebSocket.js
+++ frontend/src/hooks/useWebSocket.js
@@ -1,11 +1,11 @@
 import { useEffect, useState, useRef } from 'react';
 
-const useWebSocket = (url, onSessionEnd) => {
+const useWebSocket = (url, planCode, onSessionEnd) => {
   const [timeLeft, setTimeLeft] = useState(null);
   const socket = useRef(null);
   const [reconnectAttempts, setReconnectAttempts] = useState(0);
   const maxReconnectAttempts = 2;
-  const [sessionActive, setSessionActive] = useState(true);
+  const [sessionActive, setSessionActive] = useState(null);
 
   const connect = () => {
     if (reconnectAttempts >= maxReconnectAttempts) {
@@ -18,17 +18,27 @@ const useWebSocket = (url, onSessionEnd) => {
 
     socket.current.onopen = () => {
       console.log('WebSocket Connected');
+      // Request the current session state when the connection is established:
+      sendMessage({ action: 'check-session-state', planCode });
       setReconnectAttempts(0);
     };
 
     socket.current.onmessage = (event) => {
       const message = JSON.parse(event.data);
-      if (message.type === 'time-left') {
-        setTimeLeft(message.duration);
-      } else if (message.type === 'end-voting') {
-        setSessionActive(false);
-        if (onSessionEnd) {
-          onSessionEnd();
+      if (message.planCode === planCode) {   
+        switch (message.type) {
+          case 'time-left':
+            setTimeLeft(message.duration);
+          break;
+          case 'end-voting':
+            // console.log('Received end-voting message for:', planCode);
+            setSessionActive(false);
+            if (onSessionEnd) {
+              onSessionEnd();
+            }
+            break; 
+          default:
+            console.log('Received an unknown message type:', message.type);
         }
       }
     };
@@ -45,17 +55,17 @@ const useWebSocket = (url, onSessionEnd) => {
 
   const attemptReconnect = () => {
     if (reconnectAttempts < maxReconnectAttempts) {
-      const backoffTime = Math.pow(reconnectAttempts, 2) * 100;
+      const backoffTime = Math.pow(reconnectAttempts, 2) * 1000;
       setTimeout(connect, backoffTime);
-      setReconnectAttempts((prev) => prev + 1);
+      setReconnectAttempts(reconnectAttempts + 1);
     } else {
-      console.log('Stop attempting to reconnect.');
+      console.log('Stopped attempting to reconnect.');
     }
   };
 
   const sendMessage = (message) => {
     if (socket.current && socket.current.readyState === WebSocket.OPEN) {
-      socket.current.send(message);
+      socket.current.send(JSON.stringify(message));
     } else {
       console.error('WebSocket is not open. Message not sent.');
     }
@@ -69,7 +79,7 @@ const useWebSocket = (url, onSessionEnd) => {
       }
     };
     // eslint-disable-next-line
-  }, [url]);
+  }, [url, planCode]);
 
   return { timeLeft, sessionActive, sendMessage, socket };
 };
